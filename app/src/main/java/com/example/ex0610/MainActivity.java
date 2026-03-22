aaf51ff21b827f4b3300b21809b8828a8eb98075
lv5207 <lv5207@bs.amalnet.k12.il>
2026-03-22 20:40:30 +0200

    Second commit

--- app/src/main/java/com/example/ex0610/MainActivity.java
+++ app/src/main/java/com/example/ex0610/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex0610;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +14,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+private ToggleButton tb;
+private Switch sw;
+private Button btn;
+private LinearLayout llMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tb = findViewById(R.id.tb);
+        sw = findViewById(R.id.sw);
+        btn = findViewById(R.id.btn);
+        llMain = findViewById(R.id.llMain);
+    }
+
+    /**
+     * Changes the application background according to the states (on or off) of the toggle and switch.
+     * In total, there are four backgrounds possible.
+     * <p>
+     *
+     * @param view The button being clicked.
+     */
+    public void checkOnOff(View view) {
+        boolean tbOn = tb.isChecked();
+        boolean swOn = sw.isChecked();
 
     }
 }
